0abe553ef822c26dcd29918a42f06bb794f0750d
mnaumoski <naumoski.marija@gmail.com>
2016-09-24 13:35:42 -0400

    change in js

--- assets/Javascript/ticketmaster.js
+++ assets/Javascript/ticketmaster.js
@@ -2,8 +2,25 @@ console.log("loaded");
 
 var queryURL = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=cAnARp0Bdf5EhZsX9PCXuiOGzhkmhFM7"
 
-$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+$.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
 
-console.log(response);
+
+    console.log(response);
+    console.log(response._embedded);
+    console.log(response._embedded.events);
+    for (var i=0; i < 10; i++) {
+
+        var listDiv = $('<div>');
+        var item = $('<p>');
+        item.attr('src', response._embedded.events.response[i]);
+
+        //Add rating
+        // var rating = response.data[i].rating;
+        // var putRatingHere = $('<p>').text("This giphy's rating: " + rating);
+        // giphyDiv.append(image);
+        // giphyDiv.prepend(putRatingHere);
+        // $('#animalsView').append(giphyDiv);
+  }
+
 
 })

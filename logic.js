d11331a013fb16c709550efef6214a13d7fa6e75
Marija Naumoski <bonfoatoo@yahoo.co.uk>
2016-09-24 14:08:29 -0400

    Update logic.js

---
+++ logic.js
@@ -0,0 +1,9 @@
+var queryURL = "https://crossorigin.me/https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=museums&location=61.205414,-149.891435&radius=50000&key=AIzaSyB57N6LfRt0KPZjKV9f0U-rxCaNF4KLL-k";
+      
+
+    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+    var results = response.data;
+
+    console.log(queryURL);
+      console.log(response);
+    })
